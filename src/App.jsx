b5bef72f649a06cc1f4e8648d818480fd64321de
isnendyankp <isnendyankp@gmail.com>
2023-10-09 11:27:07 +0700

    s7-219:Add h4 to section

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ const App = () => {
   const [count, setCount] = useState(1);
   return (
     <section className="section-center">
-      Lorem Ipsum
+      <h4>tired of boring lorem ipsum</h4>
     </section>
   );
 };
@@ -12,3 +12,4 @@ export default App;
 // - s7-219:Cr8 count state value
 // - s7-219:Change h2 to section @return
 // - s7-219:Add cN section-center to section
+// - s7-219:Add h4 to section
