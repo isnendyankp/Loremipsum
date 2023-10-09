b45c91789a91795fdf8cdf008df4b7593540fc70
isnendyankp <isnendyankp@gmail.com>
2023-10-09 11:26:08 +0700

    s7-219:Add cN section-center to section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 const App = () => {
   const [count, setCount] = useState(1);
   return (
-    <section>Lorem Ipsum</section>
+    <section className="section-center">
+      Lorem Ipsum
+    </section>
   );
 };
 export default App;
@@ -9,3 +11,4 @@ export default App;
 // progress:
 // - s7-219:Cr8 count state value
 // - s7-219:Change h2 to section @return
+// - s7-219:Add cN section-center to section
