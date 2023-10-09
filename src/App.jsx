ff9865b594ee357381cb2a8b5e6853ee32eb4729
isnendyankp <isnendyankp@gmail.com>
2023-10-09 11:20:23 +0700

    s7-219:Change h2 to section @return

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 const App = () => {
   const [count, setCount] = useState(1);
-  return <h2>Lorem Ipsum Starter</h2>;
+  return (
+    <section>Lorem Ipsum</section>
+  );
 };
 export default App;
 
 // progress:
 // - s7-219:Cr8 count state value
+// - s7-219:Change h2 to section @return
